9aabe8ce6ca13c0394429a4a005fc54072e5bf1d
Sc1pex <scipex.dev@gmail.com>
2024-07-20 13:42:58 +0300

    unicode char support

--- src/driver/uart.rs
+++ src/driver/uart.rs
@@ -46,29 +46,7 @@ impl UARTDriverInner {
         }
     }
 
-    fn read_char_blocking(&self) -> char {
-        while mmio::read_bits(FR, FR_RXFE, 1) == 1 {
-            unsafe {
-                asm!("nop");
-            }
-        }
-
-        match mmio::read(DR) as u8 as char {
-            '\r' => '\n',
-            c => c,
-        }
-    }
-
-    fn read_char(&self) -> Option<char> {
-        if mmio::read_bits(FR, FR_RXFE, 1) == 1 {
-            return None;
-        }
-        Some(self.read_char_blocking())
-    }
-}
-
-impl Write for UARTDriverInner {
-    fn write_char(&mut self, c: char) -> core::fmt::Result {
+    fn write(&mut self, c: u8) {
         while mmio::read_bits(FR, FR_TXFF, 1) == 1 {
             unsafe {
                 asm!("nop");
@@ -76,12 +54,33 @@ impl Write for UARTDriverInner {
         }
 
         mmio::write(DR, c as u32);
-        Ok(())
     }
 
+    fn read_blocking(&self) -> u8 {
+        while mmio::read_bits(FR, FR_RXFE, 1) == 1 {
+            unsafe {
+                asm!("nop");
+            }
+        }
+
+        match mmio::read(DR) as u8 {
+            b'\r' => b'\n',
+            c => c,
+        }
+    }
+
+    fn read(&self) -> Option<u8> {
+        if mmio::read_bits(FR, FR_RXFE, 1) == 1 {
+            return None;
+        }
+        Some(self.read_blocking())
+    }
+}
+
+impl Write for UARTDriverInner {
     fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        for c in s.chars() {
-            self.write_char(c)?;
+        for c in s.bytes() {
+            self.write(c);
         }
         Ok(())
     }
@@ -99,22 +98,72 @@ impl UARTDriver {
 
     pub fn write_char(&self, c: char) {
         // Cannot return an error
-        let _ = self.inner.lock(|i| i.write_char(c));
+        let _ = self.inner.lock(|i| {
+            let mut b = [0; 4];
+            let s = c.encode_utf8(&mut b);
+            for c in s.bytes() {
+                i.write(c);
+            }
+        });
     }
     pub fn write_str(&self, s: &str) {
         let _ = self.inner.lock(|i| i.write_str(s));
     }
+
     /// Block until the transmit FIFO is empty
     pub fn flush(&self) {
         self.inner.lock(|i| i.flush());
     }
+
+    /// Block until a u8 is sent
+    pub fn read_blocking(&self) -> u8 {
+        self.inner.lock(|i| i.read_blocking())
+    }
+    /// Try to read a u8 without blocking
+    pub fn read(&self) -> Option<u8> {
+        self.inner.lock(|i| i.read())
+    }
+
     /// Block until a char is sent
     pub fn read_char_blocking(&self) -> char {
-        self.inner.lock(|i| i.read_char_blocking())
+        self.inner.lock(|i| {
+            let mut b = [0; 4];
+            for idx in 0..4 {
+                b[idx] = i.read_blocking();
+
+                if let Ok(c) = core::str::from_utf8(&b[0..idx]) {
+                    // TODO: Check that str has lenght of 1?
+                    return c.chars().next().unwrap();
+                }
+            }
+
+            // TODO: Return an error
+            char::REPLACEMENT_CHARACTER
+        })
     }
-    /// Try to read a char without blocking
+    /// Try to read a char
+    /// If a multi byte char is read will block until all bytes are read
     pub fn read_char(&self) -> Option<char> {
-        self.inner.lock(|i| i.read_char())
+        self.inner.lock(|i| {
+            let mut b = [0; 4];
+            b[0] = i.read()?;
+            if let Ok(c) = core::str::from_utf8(&b[0..=1]) {
+                // TODO: Check that str has lenght of 1?
+                return Some(c.chars().next().unwrap());
+            }
+
+            for idx in 1..4 {
+                b[idx] = i.read_blocking();
+
+                if let Ok(c) = core::str::from_utf8(&b[0..=idx]) {
+                    // TODO: Check that str has lenght of 1?
+                    return Some(c.chars().next().unwrap());
+                }
+            }
+
+            // TODO: Return an error
+            None
+        })
     }
 
     pub fn write_fmt(&self, args: core::fmt::Arguments) -> core::fmt::Result {
@@ -127,4 +176,3 @@ impl Driver for UARTDriver {
         Ok(())
     }
 }
-
