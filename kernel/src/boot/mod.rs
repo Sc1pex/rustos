b2bead511d5a12937a422ee2e26abc5d77991314
Sc1pex <scipex.dev@gmail.com>
2024-08-30 15:00:35 +0300

    use aarch64_cpu to jump to el1

--- kernel/src/boot/mod.rs
+++ kernel/src/boot/mod.rs
@@ -1,7 +1,6 @@
 use crate::kernel_init;
+use aarch64_cpu::registers::*;
 use core::arch::{asm, global_asm};
-
-use crate::write_reg;
 
 global_asm!(include_str!("boot.S"));
 
@@ -14,28 +13,21 @@ pub unsafe extern "C" fn __start_rust(sp_addr: u64) -> ! {
 }
 
 unsafe fn prepare_jump_to_el1(sp_addr: u64) {
-    // Don't trap access to timer counter registers in EL1 or EL0
-    write_reg!("CNTHCTL_EL2", 0b11);
+    CNTHCTL_EL2.write(CNTHCTL_EL2::EL1PCEN::SET + CNTHCTL_EL2::EL1PCTEN::SET);
+    CNTVOFF_EL2.set(0);
 
-    // Set timer counter offset to 0
-    write_reg!("CNTVOFF_EL2", 0);
+    CPACR_EL1
+        .write(CPACR_EL1::FPEN::TrapNothing + CPACR_EL1::TTA::NoTrap + CPACR_EL1::ZEN::TrapNothing);
+    HCR_EL2.write(HCR_EL2::RW::EL1IsAarch64);
 
-    // Don't trap accesses to SVE, Advanced SIMD, and
-    // floating-point registers in EL0 or EL1
-    write_reg!("CPACR_EL1", 0b11 << 20);
+    SPSR_EL2.write(
+        SPSR_EL2::F::Masked
+            + SPSR_EL2::I::Masked
+            + SPSR_EL2::A::Masked
+            + SPSR_EL2::D::Masked
+            + SPSR_EL2::M::EL1h,
+    );
 
-    // Run in aarch64 mode, not aarch32
-    write_reg!("HCR_EL2", 1 << 31);
-
-    // Mask all asynchronous interrupts
-    let mut spsr: u64 = 0b1111 << 6;
-    // Use SP_EL1 as the stack pointer
-    spsr |= 0b0101;
-    write_reg!("SPSR_EL2", spsr);
-
-    // Set address to jump after 'eret'
-    write_reg!("ELR_EL2", kernel_init as *const () as u64);
-
-    // Reuse the stack pointer of EL2 for EL1, since we never return to EL2
-    write_reg!("SP_EL1", sp_addr);
+    ELR_EL2.set(kernel_init as *const () as u64);
+    SP_EL1.set(sp_addr);
 }
