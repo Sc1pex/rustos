6ec05308279c5dca19b42e2989b8c8e19b7c4760
Sc1pex <scipex.dev@gmail.com>
2024-08-30 15:23:22 +0300

    use aarch64_cpu in mmu code

--- kernel/src/memory/mmu.rs
+++ kernel/src/memory/mmu.rs
@@ -1,6 +1,5 @@
 use super::translation_table::KERNEL_TABLES;
-use crate::{read_reg, write_reg};
-use core::arch::asm;
+use aarch64_cpu::{asm::barrier, registers::*};
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -15,9 +14,7 @@ pub unsafe fn enable() -> Result<(), MMUEnableError> {
         return Err(MMUEnableError::AlreadyEnabled);
     }
 
-    // Make sure cpu supports 64k granules
-    let id_aa64mmfr0 = read_reg!("ID_AA64MMFR0_EL1");
-    if (id_aa64mmfr0 & (0b1111 << 24)) != 0 {
+    if !ID_AA64MMFR0_EL1.matches_all(ID_AA64MMFR0_EL1::TGran64::Supported) {
         return Err(MMUEnableError::Granule64KNotSupported);
     }
 
@@ -27,48 +24,41 @@ pub unsafe fn enable() -> Result<(), MMUEnableError> {
         .populate()
         .map_err(|e| MMUEnableError::Other(e))?;
 
-    write_reg!("TTBR0_EL1", KERNEL_TABLES.phys_base_addr());
+    TTBR0_EL1.set_baddr(KERNEL_TABLES.phys_base_addr());
 
     configure_tcr();
 
     // Turn on MMU
-    let mut sctlr: u64 = 1; // MMU on
-    sctlr |= 1 << 2; // Cacheable memory
-    sctlr |= 1 << 12; // Also something about caching
-    write_reg!("SCTLR_EL1", sctlr);
-    asm!("isb sy");
+    SCTLR_EL1.write(SCTLR_EL1::M::Enable + SCTLR_EL1::C::Cacheable + SCTLR_EL1::I::Cacheable);
+    barrier::isb(barrier::SY);
 
     Ok(())
 }
 
 pub fn is_enabled() -> bool {
-    (read_reg!("SCTLR_EL1") & 1) == 1
+    SCTLR_EL1.matches_all(SCTLR_EL1::M::Enable)
 }
 
 unsafe fn configure_tcr() {
-    let mut tcr: u64 = 0;
-
     let num_bits = (super::map::END_INCLUSIVE + 1).trailing_zeros();
     let t0sz = (64 - num_bits) as u64;
-    assert!(t0sz <= 0x3F, "t0sz must fit in 6 bits");
 
-    tcr |= 0b010 << 32; // 40 bit IPA
-    tcr |= 0b01 << 14; // 64 Kib Granule size
-    tcr |= 0b11 << 12; // Inner sharable
-    tcr |= 0b01 << 10; // Outer chachable normal memory
-    tcr |= 0b01 << 8; // Inner chachable normal memory
-    tcr |= 0b1 << 23; // Disable TTBR1 Walks
-    tcr |= t0sz & 0x3F;
-
-    write_reg!("TCR_EL1", tcr);
+    TCR_EL1.write(
+        TCR_EL1::IPS::Bits_40
+            + TCR_EL1::IRGN0::WriteBack_ReadAlloc_WriteAlloc_Cacheable
+            + TCR_EL1::ORGN0::WriteBack_ReadAlloc_WriteAlloc_Cacheable
+            + TCR_EL1::SH0::Inner
+            + TCR_EL1::TG0::KiB_64
+            + TCR_EL1::EPD1::DisableTTBR1Walks
+            + TCR_EL1::EPD0::EnableTTBR0Walks
+            + TCR_EL1::T0SZ.val(t0sz),
+    );
 }
 
 fn setup_mair() {
-    // Attribute 1 - Cacheable DRAM
-    let attrib1: u64 = 0b1111_1111; // Write-back non-transistent, alloc read and write for inner and outer
-
-    // Attribute 0 - Device memory
-    let attrib0: u64 = 0b0000_0100; // non-gathering non-reordering non-cacheable
-
-    write_reg!("MAIR_EL1", (attrib0 | (attrib1 << 8)));
+    MAIR_EL1.write(
+        MAIR_EL1::Attr0_Device::nonGathering_nonReordering_EarlyWriteAck
+            + MAIR_EL1::Attr1_Normal_Inner::WriteBack_NonTransient_ReadWriteAlloc
+            + MAIR_EL1::Attr1_Normal_Outer::WriteBack_NonTransient_ReadWriteAlloc,
+    );
 }
